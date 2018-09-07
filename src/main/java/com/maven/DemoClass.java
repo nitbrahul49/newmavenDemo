a3b799932289da513256f759b571c9403efa44c3
Rahul Meena <rahul.meena@mindtree.com>
2018-09-08 01:08:44 +0530

    test cases added

---
+++ src/main/java/com/maven/DemoClass.java
@@ -0,0 +1,10 @@
+package com.maven;
+
+public class DemoClass {
+
+	
+	public int addTwoNumbers(int a, int b)
+	{
+		return a +b;
+	}
+}
---
+++ src/test/java/com/maven/DemoClassTest.java
@@ -0,0 +1,17 @@
+package com.maven;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DemoClassTest {
+
+	@Test
+	public void testAddTwoNumbers() {
+		//fail("Not yet implemented");
+		DemoClass demo= new DemoClass();
+		int r = demo.addTwoNumbers(100, 200);
+		assertEquals(300, r);
+	}
+
+}
